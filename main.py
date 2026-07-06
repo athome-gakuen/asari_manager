a040103739f38fdf6cb855fe65dbd7264730d762
Suzuki Takaaki <c1400898@st.kanazawa-it.ac.jp>
2026-07-06 11:59:08 +0900

    バグを修正

--- main.py
+++ main.py
@@ -27,7 +27,6 @@ BOTS = {
 class AsariManager(discord.Client):
     def __init__(self):
         intents = discord.Intents.default()
-        intents.members = True
         super().__init__(intents=intents)
         self.tree = app_commands.CommandTree(self)
 
